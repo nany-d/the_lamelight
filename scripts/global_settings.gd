cc964b907648ea5a7e7304793976e8c553182dd8
Danny Martin <dannymartin@192.168.1.100>
2024-01-27 01:31:00 +0000

    git marge complete

---
+++ scripts/global_settings.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+@export var comedy = 0
+@export var difficulty = 10
+@export var punch_number = 3
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,11 +1,11 @@
 extends Control
 
 @onready var joke = $Joke
+@onready var falling_punchlines = $FallingPunchlines
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	joke.visible = false
-	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +14,6 @@ func _process(_delta):
 
 
 func _on_button_pressed():
+	falling_punchlines.spawn_punchlines()
 	joke.visible = true
 	joke.next_phrase()
