c8bf5cc74297a31280774708c9c047aa41e085ed
Jakr <73387884+Jakmarting@users.noreply.github.com>
2024-01-27 20:59:34 +0000

    My MVP additions

---
+++ scripts/censored_line.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var censored_button = $CensoredButton
+@onready var visible_on_screen_notifier_2d = $VisibleOnScreenNotifier2D
+
+var difficulty = GlobalSettings.difficulty
+var has_been_clicked = false
+
+func _ready():
+	censored_button.connect("button_down", press_censor)
+
+func _process(delta : float):
+	# Move the potential punchlines down by 10 * the difficulty
+	position += Vector2(0, 20 * delta * difficulty)
+
+func press_censor():
+	has_been_clicked = true
