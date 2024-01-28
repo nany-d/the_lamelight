8055c274e2b0b25211cfd8decd4efd76c09d964a
Emanuele Bonura <bonura.emanuele.sv@gmail.com>
2024-01-28 15:41:45 +0000

    better shadow movement

--- ui/shadow.gd
+++ ui/shadow.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 var original_position = Vector2()
 var is_talking = false
+var is_leaving = false
 var talk_time_elapsed = 0.0
 var talk_duration = 3.0  # Duration of the talking animation in seconds
 var amplitude = 50.0  # Amplitude of the sine wave motion
@@ -20,7 +21,9 @@ func shadow_show():
 	animplayer.play("shadow_show")
 
 func shadow_hide():
+	is_leaving = true
 	animplayer.play("shadow_hide")
+
 
 # Make single shadow oscillate up and down to convey it's talking
 func start_talking():
@@ -34,7 +37,7 @@ func stop_talking():
 func _process(delta):
 	if is_talking:
 		talk_time_elapsed += delta
-		var y_offset = (sin(talk_time_elapsed * 2 * PI + phase_offset) + 1) * 0.5 * amplitude
+		var y_offset = (sin(talk_time_elapsed * 4 * PI + phase_offset) + 1) * 0.5 * amplitude
 		position.y = original_position.y - y_offset
 
 		if talk_time_elapsed >= talk_duration:
